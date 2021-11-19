2eda7c2b25050cc12fc61c7604afa70fa9df1d67
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-11-19 17:35:15 +0100

    test request to backend. Switch for environment-type

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+function onButtonClick() {
+  var backendUrl;
+  if (process.env.development) {
+    backendUrl = "https://kinoticket-backend-dev.herokuapp.com/";
+  } else if (process.env.production) {
+    backendUrl = "https://kinoticket-backend-prod.herokuapp.com/";
+  } else {
+    backendUrl = "http://localhost:8080/";
+  }
+
+  axios.get(backendUrl + "testRequest")
+  .then(function (response) {
+    alert("Requested URL was: " + backendUrl  + "\nResponse data: " + response.data);
+  })
+  .catch(function (error) {
+    alert(error);
+  });
+
+}
 
 function App() {
   return (
@@ -9,14 +30,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. :-)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={onButtonClick}>Trigger backend</button>
       </header>
     </div>
   );
