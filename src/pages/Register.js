9245b59315b488bc4094f465dc825056d8c21310
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-12-09 19:47:08 +0100

    add register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import './Register.css'
 
-const Logout = ({setIsLoggedIn}) => {
+import React, { useState } from 'react';
+import Field from '../components/TextInput/Field';
 
-  setIsLoggedIn(false)
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onRegisterButtonPress = () => {
+    alert("register")
+  }
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>Register</h1>
-    </div>
+    <div className='RegisterPageDiv'>
+        <div className='RegisterDiv'>
+          <h1 className='RegisterHeader'>Registrierung</h1>
+          <div style={{margin: "5%"}}>
+            <Field label="Email Adresse" setInputValue={setEmail}/>
+          </div>
+          <div style={{margin: "5%"}}>
+            <Field label="Password" type="password" setInputValue={setPassword}/>
+          </div>
+          <div style={{margin: "5%"}}>
+            <Field label="Name" setInputValue={setName}/>
+          </div>
+          <button className='RegisterButton' onClick={onRegisterButtonPress}>Register</button>
+        </div>
+      </div>
   );
 };
 
-export default Logout;
+export default Register;
