39a1f63ca61cf95296c40994ec562661d632f0a0
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-12-10 13:49:27 +0100

    apply background and margin to all children

--- src/components/Popup/PopupContainer.jsx
+++ src/components/Popup/PopupContainer.jsx
@@ -4,7 +4,7 @@ const PopupContainer = (props) => {
 
   return (
     <div className='PopupDiv'>
-      {props.title && <h1 className='PopupTitle'>{props.title}</h1>}
+      {props.title && <h1>{props.title}</h1>}
       {props.children}
     </div>
   );
