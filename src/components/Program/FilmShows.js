0c14374a129234c7b7e4f78c9aaabf27315361c6
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-12-12 18:44:16 +0100

    refactor to function component

--- src/components/Program/FilmShows.js
+++ src/components/Program/FilmShows.js
@@ -1,51 +1,46 @@
 import './FilmShows.css';
 
 import React from 'react';
-import { Component } from 'react';
 
-export class FilmShows extends Component {
+export const FilmShows = (props) => {
 
-  constructor(props) {
-    super(props);
-
-    this.filmShows = props.filmShows;
-    console.log(this.filmShows)
+  const handleClick = () => {
+    alert("test")
   }
 
-  render = function () {
-    if (this.filmShows && this.filmShows.length === 0) {
-      return (
-        <div className="filmShowsDiv">
-          {
-             (
-              <p className="noShowsAvailableMsg">
-                  Sorry! There are currently no Shows available. We'll be back soon.
-              </p>)
-          }    
-        </div>
-      );
-    }
-
+  if (props.filmShows && props.filmShows.length === 0) {
     return (
-      <div style={{
-        marginTop: 30
-      }}>
-        <p>Nächste Vorstellungen</p>
-        <div className="filmShowsDiv">
+      <div className="filmShowsDiv">
         {
-          this.filmShows.map((filmShow) => {
-            let date = new Date(filmShow.date).toLocaleDateString();
-            let time = filmShow.time // TODO: parse Date from timestamp
-
-            return (
-                <div className="filmShowDiv" onClick={() => alert("to booking page")}>
-                    <p>{date}</p>
-                    <p>{time}</p>
-                </div>
-            )})
-        }
-        </div>
+            (
+            <p className="noShowsAvailableMsg">
+                Sorry! There are currently no Shows available. We'll be back soon.
+            </p>)
+        }    
       </div>
     );
   }
+
+  return (
+    <div style={{
+      marginTop: 30
+    }}>
+      <p>Nächste Vorstellungen</p>
+      <div className="filmShowsDiv">
+      {
+        props.filmShows.map((filmShow) => {
+          let date = new Date(filmShow.date).toLocaleDateString();
+          let time = filmShow.time // TODO: parse Date from timestamp
+
+          return (
+              <div className="filmShowDiv" onClick={handleClick}>
+                  <p>{date}</p>
+                  <p>{time}</p>
+              </div>
+          )})
+      }
+      </div>
+    </div>
+  );
 }
+
