d970623a357f88903b65e4235b9f73e4211a2bd3
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-12-13 00:44:35 +0100

    implement booking

---
+++ src/components/Booking/CheckoutArea.jsx
@@ -0,0 +1,53 @@
+import './CheckoutArea.css';
+import PopupContinueButton from '../Popup/PopupContinueButton';
+import { reserveSeats } from '../../api';
+
+const CheckoutArea = ({selectedSeats, filmShowId}) => {
+
+  const onButtonClick = () => {
+    async function postToApi()  {
+        let seats = [];
+        selectedSeats.map((seat) => seats.push(seat.seat));
+        console.log(seats)
+        await reserveSeats(seats, filmShowId).then(response => {
+          if (response === false) {
+            alert("Error! Please refresh and try again");
+          } else if (response === true) {
+            alert("Success!");
+          }
+        })
+    }
+    postToApi();
+  }
+
+  function compare(a, b) {
+    if (a.seat.row < b.seat.row) {
+      return -1;
+    } else if (a.seat.row > b.seat.row) {
+      return 1;
+    } else {
+      return (a.seat.seatNumber - b.seat.seatNumber);
+    }
+  }
+  
+  selectedSeats.sort(function(a, b) {
+    return compare(a, b);
+  });
+
+  return (
+    <div className='checkoutArea'>
+      <ul>
+        {selectedSeats.map(
+          (seat) => {
+            return (
+              <li>Row {seat.seat.row}, Nr {seat.seat.seatNumber}</li>
+            )
+          }
+        )}
+      </ul>
+      <PopupContinueButton onClick={onButtonClick}>Checkout</PopupContinueButton>
+    </div>
+  );
+}
+
+export default CheckoutArea;
--- src/components/Program/FilmShows.js
+++ src/components/Program/FilmShows.js
@@ -1,21 +1,19 @@
 import './FilmShows.css';
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const FilmShows = (props) => {
-
-  const handleClick = () => {
-    alert("test")
-  }
+  const navigate = useNavigate();
 
   if (props.filmShows && props.filmShows.length === 0) {
     return (
       <div className="filmShowsDiv">
         {
-            (
-            <p className="noShowsAvailableMsg">
-                Sorry! There are currently no Shows available. We'll be back soon.
-            </p>)
+          (
+          <p className="noShowsAvailableMsg">
+            Sorry! There are currently no Shows available. We'll be back soon.
+          </p>)
         }    
       </div>
     );
@@ -29,6 +27,10 @@ export const FilmShows = (props) => {
       <div className="filmShowsDiv">
       {
         props.filmShows.map((filmShow) => {
+          const handleClick = () => {          
+            navigate("/booking/" + filmShow.id);
+          }
+
           let date = new Date(filmShow.date).toLocaleDateString();
           let time = filmShow.time // TODO: parse Date from timestamp
 
