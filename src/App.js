0d2b492c3f158b0ebb0d7514bc7834eca3e4cc9d
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-12-12 17:45:58 +0100

    Vorschlag: Die Movie ID mittels URL übergeben und dann nochmal den API call neu machen. Hat den Vorteil, dass wir weniger States hin und her schieben müssen, aber den nachteil, dass wir nochmal nen api request machen müssen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
 import Home from './pages';
 import Program from './pages/program';
 import Movie from './pages/movie';
@@ -8,19 +8,16 @@ import Signin from './pages/signin';
 import Booking from './pages/booking';
 import { useState } from 'react';
 
-
-
-
 function App() {
 
-const [movie, setMovie] = useState();
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path='/' exact element={<Home />} />
-        <Route path='/program' exact element={<Program setMovie={setMovie}/>} />
-        <Route path='/movie' exact element={<Movie movie={movie}/>}/>
+        <Route path='/program' exact element={<Program />} />
+        <Route path='/movie' exact element={<Movie />}/>
+        <Route path='/movie/:id' exact element={<Movie />}/>
         <Route path='/signin' exact element={<Signin />} />
         <Route path='/booking' exact element={<Booking />} />
       </Routes>
