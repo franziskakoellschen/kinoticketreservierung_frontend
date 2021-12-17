4e058a676376607a9cbe5164a1e878ba01fb4a51
rafu07 <77190565+rafu07@users.noreply.github.com>
2021-12-18 00:07:29 +0100

    fixing regex
    fixing regex
    
    format code
    
    Feature/film show information (#22)
    
    * add filmshow infos to booking page
    
    * refactor
    
    Co-authored-by: Niklas Weidenfeller <niklas.weidenfeller@sap.com>
    
    fix some warning, reformat code, cleanup console logs
    
    added css
    
    fix display issues
    
    changed woding, set startpage etc.
    
    checkregex
    
    fix flackering
    
    chaning standard picc
    
    helllo

--- src/pages/movie.js
+++ src/pages/movie.js
@@ -20,6 +20,7 @@ const Movie = ({ route, navigation }) => {
   const [filmShowState, setFilmShowState] = useState();
   const [filmShowsValues, setFilmShowsValues] = useState([]);
   const [filmShowKeys, setFilmShowKeys] = useState([]);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   useEffect(() => {
     
@@ -102,7 +103,43 @@ const Movie = ({ route, navigation }) => {
     }
   }
 
-  const [image, setImage] = useState('/9j/4AAQSkZJRgABAQEAeAB4AAD/4QAiRXhpZgAATU0AKgAAAAgAAQESAAMAAAABAAEAAAAAAAD/2wBDAAIBAQIBAQICAgICAgICAwUDAwMDAwYEBAMFBwYHBwcGBwcICQsJCAgKCAcHCg0KCgsMDAwMBwkODw0MDgsMDAz/2wBDAQICAgMDAwYDAwYMCAcIDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAz/wAARCAAKAAoDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD8V6KKKAP/2Q==');
+  function sortFilmShows() {
+    let showMap = new Map();
+    if (movie !== undefined) {
+      const filmShows = movie.filmShows;
+
+      filmShows.map(currentShow => {
+
+        var date = new Date(currentShow.date);
+
+          var filmShowArray = [];
+          filmShowArray.push(currentShow);
+          var stringDate = date.toString().substring(0, 15);
+
+          if (showMap.has(stringDate)) {
+            showMap.get(stringDate).push(currentShow);
+          }
+          else {
+            showMap.set(stringDate, filmShowArray)
+          }
+        
+      })
+      setFilmShowState(showMap);
+      console.log('is Set');
+    }
+  }
+
+  function setFilmShowKeysAndValues() {
+    if (filmShowKeys.length === 0) {
+
+      filmShowState && filmShowState.forEach((value, key) => {
+        setFilmShowKeys(prevFilmShowKeys => [...prevFilmShowKeys, key]);
+        setFilmShowsValues(prevFilmShowValues => [...prevFilmShowValues, value]);
+      })
+    }
+  }
+
+  const [image, setImage] = useState('iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAEnQAABJ0Ad5mH3gAAAAcSURBVChTYxQTE/vPQARggtIEwahCvIBIhQwMAA9WAVUQUjPOAAAAAElFTkSuQmCC');
   const contentType = 'image/png';
   const b64Data = 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
 
